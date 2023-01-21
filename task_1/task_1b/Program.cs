f6f7981fff1713c48a47aa9fde7835bf3ba80339
Robert <robertosa@bk.ru>
2023-01-21 21:00:31 +0300

    Systems typs

--- task_1/task_1b/Program.cs
+++ task_1/task_1b/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿int randomInt = 23;
+long randomLong = 2342;
+sbyte randomSByte = 11;
+byte randomByte = 100;
+uint randomUInt = 1123;
+float randomFloat = 14.34F;
+double randomDouble = 123.234D;
+
+randomLong= randomInt;
+randomInt = randomSByte;
+Console.WriteLine($"Long-{randomLong};Int-{randomInt};");
+randomDouble = randomFloat;
+randomLong = randomUInt;
+randomInt = randomByte;
+Console.WriteLine($"Long-{randomLong};Int-{randomInt};Double-{randomDouble};");
+
+randomInt = (int)randomFloat;
+Console.WriteLine($"Int-{randomInt}");
+randomLong = (long)randomDouble;
+randomInt = (int)randomLong;
+randomSByte=(sbyte)randomInt;
+Console.WriteLine($"Int-{randomInt};SByte-{randomSByte}");
+randomFloat=(float)randomDouble;
+Console.WriteLine($"Float-{randomFloat};");
+
+
