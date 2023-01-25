3f26b9e538bbf7effbe7286f40f2993e1b099780
Robert <robertosa@bk.ru>
2023-01-25 18:14:41 +0300

    MultiplicationCalculated

--- task_2/MultiplicationCalculated/Program.cs
+++ task_2/MultiplicationCalculated/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void MultiplicationCalculated()
+{
+    while (true) 
+    {
+        Console.WriteLine("Введите первое число которое лежит в диапазоне от 0 до 10 включительно");
+        double firsValue = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите второе число которое лежит в диапазоне от 0 до 10 включительно");
+        double secondValue = Convert.ToDouble(Console.ReadLine());
+        if (firsValue >= 0 && firsValue <= 10 && secondValue >= 0 && secondValue <= 10)
+        {
+            Console.WriteLine($"Произведение ваших чисел = {firsValue * secondValue}");
+            return;
+        }
+        else
+        {
+            Console.WriteLine($"Введеные числа не соответствуют запросу.Прочтите условие ввода внимательнее и попытайтесь ещё раз :)");
+        }
+    }
+}
+MultiplicationCalculated();
