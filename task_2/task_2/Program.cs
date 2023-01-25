a5854b19e0961956558bfbef61d4d7b56f4454b1
Robert <robertosa@bk.ru>
2023-01-25 17:53:07 +0300

    HypotenuseCaculated

---
+++ task_2/task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿ void HypotenuseCalculated()
+{
+    Console.WriteLine("Введите первый размер катета");
+    double firsCathetus=Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите второй катет");
+    double secondCathetus = Convert.ToDouble(Console.ReadLine());
+    double hypotenuse = Math.Sqrt(Math.Pow(firsCathetus,2)+Math.Pow(secondCathetus,2));
+    Console.WriteLine($"Гипотенуза в вашем прямоугольном треугольнике будет равна {hypotenuse}");
+}
+HypotenuseCalculated();
