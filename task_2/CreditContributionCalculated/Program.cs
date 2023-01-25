102882ffa8ed08d38cc61d66b1536263ac8fb94d
Robert <robertosa@bk.ru>
2023-01-25 18:49:19 +0300

    MultiplicationTable

---
+++ task_2/CreditContributionCalculated/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите сумму вклада");
+decimal contributionSumm = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Введите расчётное кол-во месяцев по вкладу");
+int monthCount=Convert.ToInt32(Console.ReadLine());
+if (contributionSumm>0 && monthCount>0)
+{
+	for (int i = 0; i < monthCount; i++)
+	{
+        contributionSumm *= 1.07M;
+    }
+}
+else
+{
+    Console.WriteLine("Проверьте введенные вами данные");
+}
--- task_2/MultiplicationTable/Program.cs
+++ task_2/MultiplicationTable/Program.cs
@@ -1,11 +1,15 @@
-﻿Console.WriteLine(" Таблица умножения целых чисел");
-Console.WriteLine("Введите число до которого неоходимо строить таблицу умножения");
-int maxValue=Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= maxValue; i++)
+﻿void MultiplicationTable()
 {
-	for (int j = 1; j <= maxValue; j++)
-	{
-		Console.Write($"{i*j} \t" );
-	}
-	Console.WriteLine(string.Empty+"\n");
+    Console.WriteLine(" Таблица умножения целых чисел");
+    Console.WriteLine("Введите число до которого неоходимо строить таблицу умножения");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    for (int i = 1; i <= maxValue; i++)
+    {
+        for (int j = 1; j <= maxValue; j++)
+        {
+            Console.Write($"{i * j} \t");
+        }
+        Console.WriteLine(string.Empty + "\n");
+    }
 }
+MultiplicationTable();
