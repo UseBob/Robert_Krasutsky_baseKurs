9ba4bc399bf7623bd7a8b575a7030152ac0bce29
Robert <robertosa@bk.ru>
2023-01-25 18:37:36 +0300

    MultiplicationTable

---
+++ task_2/MultiplicationTable/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(" Таблица умножения целых чисел");
+Console.WriteLine("Введите число до которого неоходимо строить таблицу умножения");
+int maxValue=Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= maxValue; i++)
+{
+	for (int j = 1; j <= maxValue; j++)
+	{
+		Console.Write($"{i*j} \t" );
+	}
+	Console.WriteLine(string.Empty+"\n");
+}
