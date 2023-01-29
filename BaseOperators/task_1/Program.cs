77dd2b0cdf1d542f2962ca0cc5d1bf42b849219a
Robert <robertosa@bk.ru>
2023-01-29 15:48:15 +0300

    BaseOperators

---
+++ BaseOperators/task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Вывод в столбик арифметической прогрессии с шагом в -20 едениц");
+int i = -20;
+do
+{
+    Console.WriteLine(i);
+    i -= 20;
+} while (i >= -100);
+
---
+++ BaseOperators/task_3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Калькулятор средней арифметической и суммы всех чисел указанного вами диапазона");
+Console.WriteLine("Введите первую границу диапазона");
+string firstValue = Console.ReadLine();
+Console.WriteLine("Введите вторую границу диапазона");
+string secondValue = Console.ReadLine();
+int resultSum = 0;
+int resultArithmeticMean = 0;
+int numberCount = 0;
+if (int.TryParse(firstValue,out int firstIntValue) && int.TryParse(secondValue, out int secondIntValue))
+{
+	if (firstIntValue>secondIntValue)
+	{
+		int temp=firstIntValue;
+		firstIntValue=secondIntValue;
+		secondIntValue=temp;
+    }
+	for (int i = firstIntValue; i <= secondIntValue; i++)
+	{
+		resultSum+= i;
+		resultArithmeticMean = (firstIntValue + secondIntValue) / 2; //формула для диапазона последовательных чисел
+		numberCount++;
+    }
+
+	int resultArithmeticMeanSecondFormula = resultSum / numberCount; // формула при которой значения чисел могут повторяться
+
+    Console.WriteLine($"Сумма чисел выбранного диапазона = {resultSum} \nCреднее арифмитическое = {resultArithmeticMean}");
+	Console.WriteLine($"Cреднее арифмитическое по второй формуле = {resultArithmeticMeanSecondFormula}");
+}
+else
+{
+    Console.WriteLine("Проверьте корректность данных ");
+}
