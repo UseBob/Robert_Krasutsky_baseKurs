8d853eb64a15f1c294ada39ba112c850457b7176
UserBob <robertosa@bk.ru>
2023-02-24 19:52:08 +0300

    stringMethods
    расширил возможность использования кода.

--- Base_array_string/task3/Program.cs
+++ Base_array_string/task3/Program.cs
@@ -1,8 +1,23 @@
 ﻿using System.Text;
 using System.Text.RegularExpressions;
-
-string link = "c:\\WebServers\\home\\testsite\\www\\myfile.txt";
-int myfile=link.LastIndexOf("myfile");
-//Console.WriteLine(myfile);
-string file=link.Substring(32,6);
-Console.WriteLine(file);
+Console.WriteLine( "Введите слово или фрагмент который вы желаете найти.\nЕсли фрагментов в тексте несколько, вам будет выдан первый из них.");
+string searchWord=Console.ReadLine();
+Console.WriteLine("Вставьте текст в котором желаете произвести поиск");
+string text=Console.ReadLine();
+int symbolCount = searchWord.Length;
+int mySearchIndex = text.IndexOf(searchWord);
+if (text.Contains(searchWord))
+{
+    string fragmentText = text.Substring(mySearchIndex, symbolCount);
+    Console.WriteLine(fragmentText);
+}
+else
+{
+    Console.WriteLine("В тексте не обнаружено такого фрагмента");
+}
+//string link = "c:\\WebServers\\home\\testsite\\www\\myfile.txt";
+//string test = "myfile";
+//Console.WriteLine(symbolCount);
+//int myfile=link.LastIndexOf("myfile");
+//string file=link.Substring(myfile, symbolCount);
+//Console.WriteLine(file);
