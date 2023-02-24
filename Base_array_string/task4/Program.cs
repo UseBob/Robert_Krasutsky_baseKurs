a84ce6cc0034668f1bbce6ed18ad4b89fc0e7f82
UserBob <robertosa@bk.ru>
2023-02-24 20:24:09 +0300

    word in text
    мог бы конечно прокрутить каждое слово отдельно и при наличие в слове "-" делать метод Concat ну и конечно всё в новый массив. Но мне кажется это нерациональное решение, а на другое у меня нету идей.

--- Base_array_string/task4/Program.cs
+++ Base_array_string/task4/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System.Text;
 string text = Console.ReadLine();
-
-string[] dif = { " ", ",",".","!","?","-",":",";"};
+char[] dif = { ' ', ',','.','!','?',':',';'};
 string[] strlist= text.Split(dif, StringSplitOptions.RemoveEmptyEntries);
 
 Console.WriteLine($"Количество слов = {strlist.Length}");
+
 for (int i = 0; i < strlist.Length; i++)
 {
     Console.WriteLine(strlist[i]);
